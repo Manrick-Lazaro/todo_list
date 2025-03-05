33d0ae91d86a22e9973025b5f1faac2834844c93
Manrick-Lazaro <manricksantos@gmail.com>
2025-03-05 13:23:16 -0300

    adiona opção de remover item no widteg list_item

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,31 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_list/models/todo.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoListItem extends StatelessWidget {
-  TodoListItem({super.key, required this.todo});
+  const TodoListItem({super.key, required this.todo, required this.onDelete});
 
   final Todo todo;
+  final Function(Todo) onDelete;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey[300],
-        borderRadius: BorderRadius.circular(4),
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 2),
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime), style: TextStyle(fontSize: 12)),
-          Text(
-            todo.title,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (BuildContext context) {
+                onDelete(todo);
+              },
+              backgroundColor: Color(0xFFFE4A49),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Deletar',
+            ),
+          ],
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(4),
           ),
-        ],
+
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
+                style: TextStyle(fontSize: 12),
+              ),
+              Text(
+                todo.title,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+void doNothing(BuildContext context) {}
